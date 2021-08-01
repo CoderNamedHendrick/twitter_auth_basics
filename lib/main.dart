6d00509c2631f275f384ae5053472ea24c347c24
Hendrick <sebastinesoacatp@gmail.com>
2021-08-02 00:27:36 +0100

    adding firebase json

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twitter_auth_basics/auth.dart';
 import 'package:twitter_auth_basics/sign_in_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,13 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Twitter Authentication',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Provider<AuthBase>(
+      create: (context) => Auth(),
+      child: MaterialApp(
+        title: 'Twitter Authentication',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: SignInPage(),
       ),
-      home: SignInPage(),
     );
   }
 }
--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -1,26 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twitter_auth_basics/auth.dart';
 
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Twitter Auth Basics'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          color: Colors.blueAccent,
-          disabledColor: Colors.blueAccent,
-          elevation: 8,
-          onPressed: null,
-          child: Text(
-            'Sign-in with Twitter',
-            style: TextStyle(
-              color: Colors.white,
+    final auth = Provider.of<AuthBase>(context);
+    return StreamBuilder<User>(
+        stream: auth.authStateChanges,
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Home Page"),
+                centerTitle: true,
+              ),
+              body: Center(
+                child: RaisedButton(
+                  child: Text('Sign Out'),
+                  onPressed: () async => await auth.signOut(),
+                ),
+              ),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Twitter Auth Basics'),
             ),
-          ),
-        ),
-      ),
-    );
+            body: Center(
+              child: RaisedButton(
+                color: Colors.blueAccent,
+                disabledColor: Colors.blueAccent,
+                elevation: 8,
+                onPressed: () async => await auth.signInWithTwitter(),
+                child: Text(
+                  'Sign-in with Twitter',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
