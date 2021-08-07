bf239b454610a8a54ed62eb81439d75557a60de8
Hendrick <sebastinesoacatp@gmail.com>
2021-08-07 23:00:47 +0100

    done

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitter_auth_basics/auth.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthBase>(context);
@@ -11,16 +16,29 @@ class SignInPage extends StatelessWidget {
         stream: auth.authStateChanges,
         builder: (context, snapshot) {
           if (snapshot.data != null) {
+            User user = snapshot.data;
             return Scaffold(
               appBar: AppBar(
                 title: Text("Home Page"),
                 centerTitle: true,
               ),
-              body: Center(
-                child: RaisedButton(
-                  child: Text('Sign Out'),
-                  onPressed: () async => await auth.signOut(),
-                ),
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Display Name: ${user.displayName}'),
+                  Text('User email: ${user.email}'),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  RaisedButton(
+                    child: Text('Sign Out'),
+                    onPressed: () async {
+                      await auth.signOut();
+                      setState(() {});
+                    },
+                  ),
+                ],
               ),
             );
           }
@@ -33,7 +51,10 @@ class SignInPage extends StatelessWidget {
                 color: Colors.blueAccent,
                 disabledColor: Colors.blueAccent,
                 elevation: 8,
-                onPressed: () async => await auth.signInWithTwitter(),
+                onPressed: () async {
+                  await auth.signInWithTwitter();
+                  setState(() {});
+                },
                 child: Text(
                   'Sign-in with Twitter',
                   style: TextStyle(
